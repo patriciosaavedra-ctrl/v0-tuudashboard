c16672309ac28dd3230372e225eef4355be486d3
patriciosaavedra-ctrl <patricio.saavedra@haulmer.com>
2025-10-01 14:54:53 -0300

    Updated punto-modal.tsx

--- components/punto-modal.tsx
+++ components/punto-modal.tsx
@@ -68,7 +68,7 @@ export function PuntoModal({ isOpen, editingPunto, puntoForm, onClose, onFormCha
             <select
               value={puntoForm.comuna}
               onChange={(e) => onFormChange("comuna", e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent disabled:bg-gray-100 disabled:cursor-not-allowed"
               disabled={!puntoForm.region}
             >
               <option value="">Seleccionar comuna</option>
